db2340b67ab4f1fdb0cc04d35d71fe6374453d43
hanielsen <56833124+hanielsen@users.noreply.github.com>
2020-03-07 15:30:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const colourButton = document.getElementById('colourButton');
 const connect = document.getElementById('connect');
 const deviceHeartbeat = document.getElementById('deviceHeartbeat');
 
-const primaryServiceUuid = 'e95d93af-251d-470a-a062-fa1922dfa9a8'; //'6e400001-b5a3-f393-e0a9-e50e24dcca9e';
+const primaryServiceUuid = '1800'; //'6e400001-b5a3-f393-e0a9-e50e24dcca9e';
 const receiveCharUuid = '6e400002-b5a3-f393-e0a9-e50e24dcca9e';
 const sendCharUuid = '6e400003-b5a3-f393-e0a9-e50e24dcca9e';
 
